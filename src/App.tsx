1b656deffbce3ea880eff4ca6ef5274799f91b9d
breakfasting <breakfast.ing@gmail.com>
2020-10-22 00:29:32 -0700

    toggle landing page animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useMemo, useRef, useState, useReducer } from "react";
 import styled from "styled-components";
-// import MessageList, { RefObject } from "./MessageList";
 import { Message, Event } from "./Types";
 import { motion, AnimatePresence } from "framer-motion";
 import Input from "./Input";
@@ -12,6 +11,7 @@ import { colors } from "./constants";
 import { receiveMessage } from "./reducers/actions";
 import MessageHeader from "./components/MessageHeader";
 import { useSpring, animated } from "react-spring";
+import { FaChevronLeft, FaEnvelope } from "react-icons/fa";
 
 const Face = styled.img`
   position: absolute;
@@ -55,19 +55,24 @@ const OverFace = styled.img`
 const View = styled.div`
   display: flex;
   width: 100%;
+  height: 100vh;
   font-family: "Roboto";
 `;
 
-const Main = styled.div`
+const MainWrapper = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+`;
+
+const Main = styled(motion.div)`
   flex: 1;
   display: flex;
   flex-direction: column;
-  height: 100vh;
   overflow: hidden;
 `;
 
 const Panel = styled.div`
-  width: 35%;
+  flex: 35;
   min-width: 500px;
   background-color: ${colors.charcoal};
   display: flex;
@@ -91,6 +96,7 @@ const Overlay = styled.div`
 const FlexedDiv = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
 
 const Title = styled.h1`
@@ -107,12 +113,43 @@ const SubTitle = styled(Title)`
   opacity: 0.6;
 `;
 
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  color: white;
+`;
+
+const Back = styled(FaChevronLeft)`
+  font-size: 2rem;
+  opacity: 0.8;
+  cursor: pointer;
+  user-select: none;
+  transition: all 0.3s;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
 const Email = styled.a`
+  width: 2em;
+  height: 2em;
   font-size: 1.5em;
   color: white;
-  font-family: "Oswald";
   text-decoration: none;
-  margin-top: 20px;
+  background-color: ${colors.persianGreen};
+  line-height: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  opacity: 0.8;
+  transition: all 0.3s;
+  box-shadow: 0px 20px 40px -5px rgba(0, 0, 0, 0.5);
+
+  &:hover {
+    opacity: 1;
+  }
 `;
 
 const Copyright = styled.div`
@@ -160,7 +197,7 @@ const emailBody =
   "Hi%20Danny,%0D%0A%0D%0AMy%20name%20is%20<Your%20Name>%20and%20I'm%20a%20recruiter/the%20hiring%20manager%20for%20<Your%20Company>.%0D%0A%0D%0AI%20would%20like%20to%20have%20a%20phone%20discussion%20about%20an%20Software%20Engineer/Product%20Manager%20role%20that%20we%20have%20available.%0D%0A%0D%0AI'd%20like%20to%20talk%20with%20you%20more%20about%20<Your%20Company>%20and%20your%20experience.%0D%0A%0D%0AWhat%20is%20your%20availability%20for%20a%20short%20introductory%20call?%0D%0A%0D%0ALooking%20forward%20to%20hearing%20from%20you.%0D%0A%0D%0AKind%20Regards,%0D%0A%0D%0A<Your%20Name>";
 
 const App = ({ options }: Props) => {
-  const inputRef = useRef<HTMLInputElement | null>(null)
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const reconnectCount = useRef(0);
   const [props, set] = useSpring(() => ({
     xys: [0, 0, 1],
@@ -172,14 +209,12 @@ const App = ({ options }: Props) => {
   const [messages, dispatch] = useReducer(messageReducer, initialMessages);
   const user = useMemo(generateGuid, []);
   const [typing, setTyping] = useState(false);
-  const [picture, setPicture] = useState("./danny.jpg")
-  const [caption, setCaption] = useState()
-  // const addMessageToState = (message: Message) => {
-  //   setMessages((messages) => [...messages, message]);
-  // };
+  const [picture, setPicture] = useState("./danny.jpg");
+  const [caption, setCaption] = useState();
+  const [landing, setLanding] = useState(true);
 
   const sendEvent = (event: Event) => {
-    if (inputRef.current) inputRef.current.focus()
+    if (inputRef.current) inputRef.current.focus();
     if (options.useSockets && socket.current) {
       setTimeout(() => {
         socket.current &&
@@ -286,17 +321,38 @@ const App = ({ options }: Props) => {
       <Panel>
         <Overlay>
           <FlexedDiv>
-            <div>
-              <Title>Danny Huang</Title>
-              <SubTitle>Interactive Resume</SubTitle>
-            </div>
-            <Email href={`mailto:danny.r.huang@gmail.com?subject=Interview%20Invitation%20From%20<Your%20Company>&body=${emailBody}`}>
-              Email me!
+            <Logo>
+              <AnimatePresence>
+                {!landing && (
+                  <motion.div
+                    initial={{ width: 0, opacity: 0 }}
+                    animate={{ width: 40, opacity: 1 }}
+                    transition={{ duration: 0.4 }}
+                    exit={{ width: 0, opacity: 0 }}
+                    style={{ overflow: "hidden" }}
+                  >
+                    <Back onClick={() => setLanding(true)} />
+                  </motion.div>
+                )}
+              </AnimatePresence>
+
+              <div>
+                <Title>Danny Huang</Title>
+                <SubTitle>Interactive Resume</SubTitle>
+              </div>
+            </Logo>
+            <Email
+              href={`mailto:danny.r.huang@gmail.com?subject=Interview%20Invitation%20From%20<Your%20Company>&body=${emailBody}`}
+            >
+              <FaEnvelope />
             </Email>
           </FlexedDiv>
           <Copyright>© 2020, Danny Huang, TJ McCabe and Wayne Su</Copyright>
         </Overlay>
-        <Circle style={{ transform: props.xys.interpolate(trans) }}>
+        <Circle
+          style={{ transform: props.xys.interpolate(trans) }}
+          onClick={() => setLanding(false)}
+        >
           <AnimatePresence>
             {picture !== "./danny.jpg" && (
               <motion.div
@@ -312,18 +368,29 @@ const App = ({ options }: Props) => {
           <Face src={picture} />
         </Circle>
       </Panel>
-      <Main>
-        {/* <MessageHeader /> */}
-        <MessageList>
-          {messages.map((message, index) => (
-            <MessageRow key={index} message={message} sendEvent={sendEvent} />
-          ))}
-
-          {typing && <TypingIndicator />}
-          <div ref={bottomRef} className="list-bottom"></div>
-        </MessageList>
-        <Input ref={inputRef} sendEvent={sendEvent} />
-      </Main>
+      <AnimatePresence>
+        {!landing && (
+          <Main
+            initial={{ flex: 0 }}
+            animate={{ flex: 65 }}
+            transition={{ duration: 0.4 }}
+            exit={{ flex: 0 }}
+          >
+            <MessageList>
+              {messages.map((message, index) => (
+                <MessageRow
+                  key={index}
+                  message={message}
+                  sendEvent={sendEvent}
+                />
+              ))}
+              {typing && <TypingIndicator />}
+              <div ref={bottomRef} className="list-bottom"></div>
+            </MessageList>
+            <Input ref={inputRef} sendEvent={sendEvent} />
+          </Main>
+        )}
+      </AnimatePresence>
     </View>
   );
 };
